d0db08bddd3149346fb4f0cf5a2481fe0f8d6655
Liuf <810033500@qq.com>
2020-04-07 16:17:19 +0800

    初始化

---
+++ src/main/java/com/example/demo/test/TestGitTag.java
@@ -0,0 +1,14 @@
+package com.example.demo.test;
+
+/**
+ * @author : liuf
+ * @version V1.0
+ * @title: TestGitTag
+ * @date 2020年04月07日 16:13
+ **/
+public class TestGitTag {
+
+    public static void main(String[] args) {
+        System.out.println("master git tag........");
+    }
+}
